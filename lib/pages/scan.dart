85108e1258b626a35eae9542da42ed45427a3e94
Kryštof Matějka <kx.matejka@gmail.com>
2021-02-15 23:05:42 +0100

    feat: add simple scanner

--- lib/pages/scan.dart
+++ lib/pages/scan.dart
@@ -1,11 +1,53 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
+import 'package:qr_code_scanner/qr_code_scanner.dart';
 import '../page_body.dart';
 
-class Scan extends StatelessWidget {
+class Scan extends StatefulWidget {
+  const Scan({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  _ScanState createState() => _ScanState();
+}
+
+class _ScanState extends State<Scan> {
+  Barcode result;
+  QRViewController controller;
+  final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
+
+  @override
+  void reassemble() {
+    super.reassemble();
+    if (Platform.isAndroid) {
+      controller.pauseCamera();
+    }
+    controller.resumeCamera();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return PageBody(
-      child: Text('xxx'),
+    return Column(
+      children: [
+        Expanded(
+          child: QRView(
+            key: qrKey,
+            onQRViewCreated: (QRViewController controller) {
+              controller.scannedDataStream.listen((event) {
+                print(event);
+              });
+            },
+          ),
+        ),
+      ],
     );
+  }
+
+  @override
+  void dispose() {
+    controller?.dispose();
+    super.dispose();
   }
 }
